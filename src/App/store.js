95080f1b4b205a2fa3078961e08b969b460b1afc
Salman Shaikh <sk.salman0425@gmail.com>
2022-12-30 00:25:44 +0530

    changes done in redux-scratch

--- src/App/store.js
+++ src/App/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
- 
+import counterReducer from '../features/counter/counterSlice' 
 //below we are overriding configureStore function according to our needs
 // confugureStore has first attribute reducer which can have multiple reducer 
 //and they contains state variable which we have to share with entire app or particular modules
 export default configureStore({
-    reducer:{},
+    reducer:{
+        counter: counterReducer
+    },
 })
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment,incrementByAmount } from './counterSlice'
 
 export default function Counter() {
-
-  
-    return (
-      <div>
-        <div>
-          <button aria-label="Increment value">
-            Increment
-          </button>
-          <span>{}</span>
-          <button aria-label="Decrement value">
-            Decrement
-          </button>
-        </div>
-      </div>
-    )
+  const [incrementAmount,setIncrementAmount]=useState(0)
+  const count = useSelector((state )=>{
+  console.log("global state from redux",state);
+  return state.counter.value
+  });
+  const dispatch = useDispatch()
+  console.log("Count::",count);
+  const handleOnIncrementClick=()=>{
+    dispatch(increment())
   }
+  const handleOnDecrementClick=()=>{
+    dispatch(decrement())
+  }
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={handleOnIncrementClick}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={handleOnDecrementClick}
+        >
+          Decrement
+        </button>
+        <input
+        aria-label="set increment Amount"
+        value={incrementAmount}
+        onChange={(event)=> setIncrementAmount(event.target.value)}
+        ></input>
+        <button
+          onClick={()=>
+            dispatch(
+              incrementByAmount(Number(incrementAmount)||0)
+            )}
+        >
+          Increment by Amount
+        </button>
+
+      </div>
+    </div>
+  )
+}
