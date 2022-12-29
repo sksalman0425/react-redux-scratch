89c0a102dd8f5811ff410c56bf50a91aa51b7f8d
Salman Shaikh <sk.salman0425@gmail.com>
2022-12-29 20:59:51 +0530

    changes done in redux scratch

---
+++ src/App/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit'
+ 
+//below we are overriding configureStore function according to our needs
+// confugureStore has first attribute reducer which can have multiple reducer 
+//and they contains state variable which we have to share with entire app or particular modules
+export default configureStore({
+    reducer:{},
+})
---
+++ src/features/counter/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Counter() {
+
+  
+    return (
+      <div>
+        <div>
+          <button aria-label="Increment value">
+            Increment
+          </button>
+          <span>{}</span>
+          <button aria-label="Decrement value">
+            Decrement
+          </button>
+        </div>
+      </div>
+    )
+  }
